28b4fc472d8fd4d0db645796b3a1edb09fbac477
Christine Stawitz <cstawitz@uw.edu>
2016-01-14 16:47:08 -0800

    Added function to plot mislabeling sample size by country and state

---
+++ R/plot_summary_data_stats.R
@@ -0,0 +1,41 @@
+plotSummaryData <- function(data.table){
+  #Plot world map of number of studies
+  makeSampleMap(data.table, "WorldSampSizeMap.pdf")
+} 
+
+makeSampleMap <- function(data.table, filename){
+  require("rworldmap")
+  isos <- c("ZAF", "USA", "ITA", "ESP", "GRC", "CAN", "GBR", "GBR","PRT", "IRL", "DEU", "FRA")
+  #Find each study that contains each code
+  isoVect <- matrix(rep("", nrow(percentage.table)*(length(isos))),nrow=nrow(percentage.table),ncol=length(isos))
+  totSamp <- rep(0,ncol(isoVect))
+  for(i in 1:length(as.character(unique(percentage.table$Country.of.sample)[-c(7,9,15,16)]))){
+    matchInd <- grep(as.character(unique(percentage.table$Country.of.sample))[i],percentage.table$Country.of.sample)
+    isoVect[matchInd,i]<-rep(1,length(matchInd))
+    totSamp[i] <- sum(as.numeric(isoVect[matchInd,i])*percentage.table$N.samples.with.this.label.in.this.study.[matchInd], na.rm=TRUE)
+  }
+  tblIso <- cbind(percentage.table,isoVect)
+  names(tblIso) <- c(names(percentage.table),isos)
+  totState <- rep(0,ncol(tblIso))
+  locations <- as.character(unique(tblIso$Location.of.sample))
+  states <- unlist(unique(regmatches(locations,regexec('[A-Z]{2}',locations)))[-1])
+  stateVect <- matrix(rep("", nrow(tblIso)*length(states)),nrow=nrow(tblIso),ncol=length(states))
+  totState <- rep(0,ncol(stateVect))
+  for(j in 1:length(states)){
+    matchInd <- grep(states[j],as.character(tblIso$Location.of.sample))
+    stateVect[matchInd,j]<-rep(1,length(matchInd))
+    totState[j] <- sum(as.numeric(stateVect[matchInd,j])*percentage.table$N.samples.with.this.label.in.this.study.[matchInd],na.rm=TRUE)
+  }
+  tblPlot <- cbind(tblIso, stateVect)
+  names(tblPlot) <-c(names(tblIso),states)
+  isoTot <- cbind(isos,as.numeric(totSamp))
+  isoTot <- as.data.frame(rbind(isoTot[-c(7,8),],c("GBR",sum(as.numeric(isoTot[c(7,8),2])))))
+  stateTot <- as.data.frame(cbind(states,as.numeric(totState)))
+  spdf <- joinCountryData2Map(isoTot, joinCode="ISO3", nameJoinColumn="isos")
+  pdf(filename)
+  mapCountryData(spdf, nameColumnToPlot="V2", catMethod="pretty")
+  dev.off()
+}
+
+
+
